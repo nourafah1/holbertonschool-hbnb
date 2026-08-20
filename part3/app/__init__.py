07d856f497ae3883c612a625b64c76add246f091
noura <noura.fahad2022@gmail.com>
2026-08-20 13:31:03 +0300

    Implement login with JWT authentication

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from config import config
+from flask_cors import CORS
 
 jwt = JWTManager()
 db = SQLAlchemy()
@@ -13,6 +14,7 @@ from app.api.v1 import api_v1_blueprint
 
 def create_app(config_name='default'):
     app = Flask(__name__)
+    CORS(app)
     # Load configuration
     app.config.from_object(config[config_name])
     # Initialize JWT
--- part4/scripts.js
+++ part4/scripts.js
@@ -1,8 +1,41 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
-  });
+    const loginForm = document.getElementById('login-form');
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', async (event) => {
+            event.preventDefault();
+
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            const errorMessage = document.getElementById('login-error');
+
+            try {
+                const response = await fetch(
+                    'http://127.0.0.1:5000/api/v1/auth/login',
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            email: email,
+                            password: password
+                        })
+                    }
+                );
+
+                if (response.ok) {
+                    const data = await response.json();
+
+                    document.cookie = `token=${data.access_token}; path=/`;
+
+                    window.location.href = 'index.html';
+                } else {
+                    errorMessage.textContent = 'Invalid email or password.';
+                }
+            } catch (error) {
+                errorMessage.textContent = 'Unable to connect to the server.';
+            }
+        });
+    }
+});
